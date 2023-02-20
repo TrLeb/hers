80772e72c9b6ae91a07225005369494754927ca2
TrLeb <tslebang@gmail.com>
2023-02-20 15:30:44 +0200

    first commit

--- app/src/main/java/com/tslebang/hers/Dashboard.java
+++ app/src/main/java/com/tslebang/hers/Dashboard.java
@@ -1,18 +1,32 @@
 package com.tslebang.hers;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.StorageReference;
 
 public class Dashboard extends AppCompatActivity {
     TextView nameTv;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser user;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    StorageReference storageReference;
     FirebaseAuth aUTH;
+    ImageView donateIV, requestIV, callIV, qnaIV, sosIV, locationIV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,15 +34,40 @@ public class Dashboard extends AppCompatActivity {
         setContentView(R.layout.activity_dashboard);
 
         nameTv = findViewById(R.id.name);
+        donateIV = findViewById(R.id.donateIV);
+        requestIV = findViewById(R.id.requestIV);
+        callIV = findViewById(R.id.callIV);
+        locationIV = findViewById(R.id.clinicsIV);
+        qnaIV = findViewById(R.id.qnaIV);
+        sosIV = findViewById(R.id.sosIV);
 
         aUTH = FirebaseAuth.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("Users");
+        Query query = databaseReference.orderByChild("email").equalTo(user.getEmail());
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot ds: snapshot.getChildren()){
+                    String name = ""+ ds.child("name").getValue();
+
+                    nameTv.setText(name);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
     private void checkUserStatus(){
 
         FirebaseUser user = aUTH.getCurrentUser();
         if (aUTH !=null){
-            assert user != null;
-            nameTv.setText(user.getDisplayName());
+
         }else {
             Toast.makeText(this, "No user", Toast.LENGTH_SHORT).show();
         }
--- app/src/main/java/com/tslebang/hers/Donate.java
+++ app/src/main/java/com/tslebang/hers/Donate.java
@@ -2,9 +2,31 @@ package com.tslebang.hers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.Location;
+import android.location.LocationRequest;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Donate extends AppCompatActivity {
+    private GoogleMap mMap;
+    GoogleApiClient mGoogleApiClient;
+    Location mLastLocation;
+    LocationRequest mLocationRequest;
+    private int REQUEST_CODE = 11;
+    SupportMapFragment mapFragment;
+    EditText mFullName,mItemName,mDescription,mPhone;
+    Button mSubmitBtn;
+    FirebaseAuth fAuth;
+    FirebaseDatabase firebaseDatabase;
+    String userID;
+    public static final String TAG = "TAG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
